69206ad6c8c50933b53758d6ebf177e804060f23
I Komang Sugiartha <sugiartha26@yahoo.co.id>
2019-08-22 10:37:18 +0700

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,28 @@
+package com.sugiartha.aplikasimobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Deklarasikan Variabel
+    EditText TextNama;
+    TextView Hasil;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Panggil Variabel Berdasarkan id
+        TextNama=(EditText)findViewById(R.id.TxtNama);
+        Hasil=(TextView)findViewById(R.id.Label2);
+    }
+    //Buat Method TampilNama Sesuai Dengan property Onclick pada button
+    public void TampilNama(View v){
+        Hasil.setText("Nama Anda: "+TextNama.getText());
+    }
+}
