3694196ac0944663109bb5104ff5a67db2c740d5
I Komang Sugiartha <sugiartha26@yahoo.co.id>
2020-10-04 20:23:39 +0700

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Panggil Variabel Berdasarkan id
-        //Test 123 DTS
+        //Test 123 DTS 123
         TextNama=(EditText)findViewById(R.id.TxtNama);
         Hasil=(TextView)findViewById(R.id.Label2);
     }
